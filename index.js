dae5caeeb716fbdf0dc1c0a0b47b039d60e764a6
ERIC KELLY <eakelly220@gmail.com>
2018-03-24 19:40:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ function sayHiToGrandma (string){
     return "I can\'t hear you!"
   }
   else if(string === "I love you, Grandma."){
-    return
+    return "I love you, too."
+  }
+  else {
+    return "something is wrong"
   }
 }
